fa6d324defa5998d43bb8928f0b496fd670d84a3
Mathew TJ <mathewtj2112@gmail.com>
2026-03-22 18:59:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿const scrollBtn = document.querySelector('.scroll-top');
+
+scrollBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 400) {
+    scrollBtn.style.opacity = '1';
+    scrollBtn.style.pointerEvents = 'auto';
+  } else {
+    scrollBtn.style.opacity = '0.6';
+    scrollBtn.style.pointerEvents = 'auto';
+  }
+});
